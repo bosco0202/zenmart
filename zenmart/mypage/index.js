d4c0525f66176d1433d896636f4144c5c38863ff
bosco0202 <abduvosid1177@gmail.com>
2025-03-30 13:42:44 +0900

    bosco: mypage weiw

---
+++ zenmart/mypage/index.js
@@ -0,0 +1,8 @@
+// // Checkbox holatini tekshirib, o'zgarishini kuzatish
+// document.querySelector(".switch input").addEventListener("change", function () {
+//   if (this.checked) {
+//     console.log("Tugma faol");
+//   } else {
+//     console.log("Tugma nofaol");
+//   }
+// }); // Checkbox holatini tekshirib, o'zgarishini kuzatish
